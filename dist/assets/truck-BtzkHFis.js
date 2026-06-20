0daa8799360eff82981f5355deb67fd3cb2b8c5f
Eder Ernesto Rodríguez Flores <ernesrodrigues2005@gmail.com>
2026-06-20 07:13:18 -0600

    Revert "chore: remove unused AdminShell component file"
    This reverts commit a1edaffff4ce1a309fe009866ca2d3bdd2dac99d.

--- dist/assets/truck-BtzkHFis.js
+++ dist/assets/truck-BtzkHFis.js
@@ -1,14 +1,14 @@
-import{e as c}from"./index-CXFKlWF4.js";/**
+import{c}from"./index-BIPQWvJI.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const e=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m15 9-6 6",key:"1uzhvr"}],["path",{d:"m9 9 6 6",key:"z0biqf"}]],k=c("circle-x",e);/**
+ */const a=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m15 9-6 6",key:"1uzhvr"}],["path",{d:"m9 9 6 6",key:"z0biqf"}]],k=c("circle-x",a);/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const a=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["polyline",{points:"12 6 12 12 16 14",key:"68esgv"}]],n=c("clock",a);/**
+ */const e=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["polyline",{points:"12 6 12 12 16 14",key:"68esgv"}]],n=c("clock",e);/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
